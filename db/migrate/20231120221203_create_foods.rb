15c59ab622578388c9f65012a8924985507eaa08
sergiok <sergioandresusma@hotmail.com>
2023-11-20 17:52:27 -0500

    Update Food model & migration according to PR

--- db/migrate/20231120221203_create_foods.rb
+++ db/migrate/20231120221203_create_foods.rb
@@ -4,7 +4,8 @@ class CreateFoods < ActiveRecord::Migration[7.1]
       t.string :name
       t.string :measurement_unit
       t.float :price
-      t.integer :quantity
+      t.float :quantity
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
