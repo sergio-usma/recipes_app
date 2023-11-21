331178815ce52976a6c02fef81e1048418d29a93
Gumaro Monroy <maromvz@gmail.com>
2023-11-21 17:21:50 -0600

    Generate recipe_foods controllers /w specs & views

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,20 @@
+class RecipeFoodsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
