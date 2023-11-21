fa4f49178b924d9fe9b46e2f0b213cbf9afcfe42
Gumaro Monroy <maromvz@gmail.com>
2023-11-21 17:22:14 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  devise_for :users
-  root to: "foods#index"
+  root 'users#index'
 
-  get 'foods/index'
-  get 'foods/show'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
+  resources :foods
+  resources :recipe_foods
+  resources :recipes
 end
