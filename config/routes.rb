1833b5ca42dfeb55cadf50747ee4949754512c2d
sergiok <sergioandresusma@hotmail.com>
2023-11-22 17:34:59 -0500

    Update recipe resources & public_recipes on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'public_recipes#index'
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resources :foods
+  get 'recipe_foods/new/:id', to: 'recipe_foods#new', as: 'new_recipe_food'
   resources :recipe_foods
   resources :recipes
+  resources :public_recipes, only: [:index]
 end
