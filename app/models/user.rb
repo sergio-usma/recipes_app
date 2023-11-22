ea88c2da36e65e35579e3da0abe45159920e87fb
Gumaro Monroy <maromvz@gmail.com>
2023-11-22 12:00:32 -0600

    Add 'confirmable' devise settings for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  has_many :foods
-  has_many :recipes
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
