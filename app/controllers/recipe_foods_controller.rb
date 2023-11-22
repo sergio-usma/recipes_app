1bc4db816f9f2710ae86d586b10cb24e07c6d57b
sergiok <sergioandresusma@hotmail.com>
2023-11-22 08:20:24 -0500

    FIX Rubocop offenses & stylelint

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,20 +1,14 @@
 class RecipeFoodsController < ApplicationController
   before_action :authenticate_user!
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 
-  def edit
-  end
+  def edit; end
 
-  def destroy
-  end
+  def destroy; end
 end
